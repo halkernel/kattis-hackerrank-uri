9f4f6c27ba716fdc747e2025da09995e52502947
Kennet Emerson <kennet.emerson@gmail.com>
2017-01-03 13:52:11 -0300

    Spavanac problem solved

---
+++ spavanac.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int h, m;
+	cin >> h >> m;
+	m += 15;
+	if(m/60 == 0){
+		h-=1;
+	}
+	else{
+		m = m%60;
+	}
+	if(h < 0){
+		h+=24;
+	}
+	cout << h << " " << m;
+				
+	return 0;
+}
+
