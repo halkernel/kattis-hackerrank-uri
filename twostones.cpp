91a9c3c933192c788733285c9861266eb24f2168
Kennet Emerson <kennet.emerson@gmail.com>
2017-01-13 01:44:48 -0300

    Two stones problem solved

---
+++ twostones.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <set>
+#include <string>
+
+
+using namespace std;
+
+
+int main()
+{
+    int m;
+	cin >> m;
+	if(m % 2 == 0)
+		cout << "Bob";
+	else
+		cout << "Alice";
+	return 0;
+}
+
