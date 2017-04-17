472306c8eecb8d8dd05f7f5b502ed9d42e0dbc56
Kennet Emerson <kennet.emerson@gmail.com>
2017-04-17 08:00:18 -0300

    lineup problem solved with python

---
+++ lineup.py
@@ -0,0 +1,9 @@
+num = int(input())
+names = [input() for _ in range(num)]
+
+if sorted(names) == names:
+    print('INCREASING')
+elif sorted(names, reverse=True) == names:
+    print('DECREASING')
+else:
+    print('NEITHER')
